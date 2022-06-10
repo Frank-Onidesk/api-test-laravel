b5aa9ed5e8fe8d88e508a65940abe7b7c8370e1a
Frank-Onidesk <onidesk@outlook.com>
2022-06-10 01:49:53 +0100

    Alteração 10-06 às 01:49

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,18 +1,40 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-    }
+
+        $faker = Faker::create();
+    	foreach (range(1,10) as $index) {
+	        DB::table('post_customers')->insert([
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+	            'phone' => rand(0,8),
+                'address' => Str::random(10),
+	        ]);
+	}
+
+
+
+      /*
+        DB::table('post_customers')->insert([
+            'name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'phone' => Str::random(10).'@gmail.com',
+
+        ]); */
+   }
+
 }
