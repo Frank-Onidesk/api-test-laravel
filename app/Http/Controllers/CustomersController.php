03c806266fd503f22c04f687e804a9a3fa3ac0ec
Frank-Onidesk <onidesk@outlook.com>
2022-06-14 18:37:19 +0100

    Changes 14-06-2022

---
+++ app/Http/Controllers/CustomersController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\PostCustomer;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
+class CustomersController extends Controller
+{
+    //
+    function show(Request $request){
+       $customers = DB::table('post_customers')->get();
+
+       return View::make('list')->with('list', $customers);
+    }
+
+
+
+
+}
